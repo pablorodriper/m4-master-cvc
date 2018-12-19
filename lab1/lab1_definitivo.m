39edcd011b46f1b2efddea777ba429f25f7b3fe7
pablorodriper <parope2@gmail.com>
2018-12-19 02:32:59 +0100

    Finish 3 and 4

--- lab1/lab1_definitivo.m
+++ lab1/lab1_definitivo.m
@@ -101,4 +101,241 @@ end
 %figure; imshow(I); 
 figure; imshow(uint8(I_affin)); title('Affine transformation from SVD');
 
+%% 1.3 Projective transformations (homographies)
+
+% ToDo: generate a matrix H which produces a projective transformation
+
+A  = [pi/4 0; 1 pi/6];
+
+%H = [1000*A [1 0]'; [1 0.5] 1];
+%H = [1.707 0.586 1; 2.707 8.242 2; 0.01 0.01 1];
+H = [0.6 0.001 -0.001; 0 0.9 -0.001; 0 0.1 1]
+
+if det(H) ~= 0
+    disp('OK: The matrix H is non-singular')
+else
+    disp('ERROR: The matrix H is not non-singular')
+    return
+end
+
+I2 = apply_H(I, H);
+%figure; imshow(I);
+figure; imshow(uint8(I2));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 2. Affine Rectification
+
+% choose the image points
+I=imread('Data/0000_s.png');
+A = load('Data/0000_s_info_lines.txt');
+
+% indices of lines
+i = 424;
+p1 = [A(i,1) A(i,2) 1]';
+p2 = [A(i,3) A(i,4) 1]';
+i = 240;
+p3 = [A(i,1) A(i,2) 1]';
+p4 = [A(i,3) A(i,4) 1]';
+i = 712;
+p5 = [A(i,1) A(i,2) 1]';
+p6 = [A(i,3) A(i,4) 1]';
+i = 565;
+p7 = [A(i,1) A(i,2) 1]';
+p8 = [A(i,3) A(i,4) 1]';
+
+
+% ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
+
+l1 = cross(p1, p2);
+l2 = cross(p3, p4);
+l3 = cross(p5, p6);
+l4 = cross(p7, p8);
+
+
+% show the chosen lines in the image
+figure;imshow(I);
+hold on;
+t=1:0.1:1000;
+plot(t, -(l1(1)*t + l1(3)) / l1(2), 'y');
+plot(t, -(l2(1)*t + l2(3)) / l2(2), 'y');
+plot(t, -(l3(1)*t + l3(3)) / l3(2), 'y');
+plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
+
+% ToDo: compute the homography that affinely rectifies the image
+
+%Vanishing points provided by each pair of parallel lines
+v1 = cross(l1, l2);
+v2 = cross(l3, l4);
+
+
+%Vanishing line
+l_inf = cross(v1, v2);
+l_inf = [l_inf(1)/l_inf(3) l_inf(2)/l_inf(3) 1]';
+
+A  = [2 2; -1 1];    
+H1 = [A [1 0]'; zeros(1,length(A)) ones(1,1)];
+H = [1 0 0; 0 1 0; l_inf(1) l_inf(2) l_inf(3)]; %H = H_aff*[]
+I2 = apply_H(I, H);
+figure; imshow(uint8(I2));
+
+% ToDo: compute the transformed lines lr1, lr2, lr3, lr4
+
+lr1 = H.'\l1;
+lr2 = H.'\l2;
+lr3 = H.'\l3;
+lr4 = H.'\l4;
+
+% show the transformed lines in the transformed image
+figure;imshow(uint8(I2));
+hold on;
+t=1:0.1:1000;
+plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
+plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
+plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
+plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
+
+% ToDo: to evaluate the results, compute the angle between the different pair 
+% of lines before and after the image transformation
+
+% Angles for original lines 
+%l1 and l2 are parallel in Euclidean frame
+%l3 and l4 are parallel in Euclidean frame
+%Compute normal vectors and angles from there
+
+n1 = [l1(1)/l1(3), l1(2)/l1(3)];
+n2 = [l2(1)/l2(3), l2(2)/l2(3)];
+n3 = [l3(1)/l3(3), l3(2)/l3(3)];
+n4 = [l4(1)/l4(3), l4(2)/l4(3)];
+
+cos_1 = (n1*n3')/(sqrt(n1*n1')*sqrt(n3*n3'))
+cos_2 = (n2*n4')/(sqrt(n2*n2')*sqrt(n4*n4'))
+cos_3 = (n1*n2')/(sqrt(n1*n1')*sqrt(n2*n2'))
+cos_4 = (n4*n3')/(sqrt(n4*n4')*sqrt(n3*n3'))
+
+%Angles for transformed lines
+
+nr1 = [lr1(1)/lr1(3), lr1(2)/lr1(3)];
+nr2 = [lr2(1)/lr2(3), lr2(2)/lr2(3)];
+nr3 = [lr3(1)/lr3(3), lr3(2)/lr3(3)];
+nr4 = [lr4(1)/lr4(3), lr4(2)/lr4(3)];
+
+
+cos_1_r = (nr1*nr3')/(sqrt(nr1*nr1')*sqrt(nr3*nr3'))
+cos_2_r = (nr2*nr4')/(sqrt(nr2*nr2')*sqrt(nr4*nr4'))
+cos_3_r = (nr1*nr2')/(sqrt(nr1*nr1')*sqrt(nr2*nr2'))
+cos_4_r = (nr4*nr3')/(sqrt(nr4*nr4')*sqrt(nr3*nr3'))
+
+% Angles between lines should not change when applying affine rectification
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 3. Metric Rectification
+
+%% 3.1 Metric rectification after the affine rectification (stratified solution)
+
+% ToDo: Metric rectification (after the affine rectification) using two non-parallel orthogonal line pairs
+%       As evaluation method you can display the images (before and after
+%       the metric rectification) with the chosen lines printed on it.
+%       Compute also the angles between the pair of lines before and after
+%       rectification.
+% choose the image points
+I=imread('Data/0000_s.png');
+A = load('Data/0000_s_info_lines.txt');
+
+% indices of lines
+i = 227;
+p1 = [A(i,1) A(i,2) 1]';
+p2 = [A(i,3) A(i,4) 1]';
+i = 367;
+p3 = [A(i,1) A(i,2) 1]';
+p4 = [A(i,3) A(i,4) 1]';
+i = 534;
+p5 = [A(i,1) A(i,2) 1]';
+p6 = [A(i,3) A(i,4) 1]';
+i = 576;
+p7 = [A(i,1) A(i,2) 1]';
+p8 = [A(i,3) A(i,4) 1]';
+
+
+
+% ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
+
+l1 = cross(p1, p2);
+l2 = cross(p3, p4);
+l3 = cross(p5, p6);
+l4 = cross(p7, p8);
+
+%Vanishing points provided by each pair of parallel lines
+v1 = cross(l1, l2);
+v2 = cross(l3, l4);
+
+
+%Vanishing line
+l_inf = cross(v1, v2);
+l_inf = [l_inf(1)/l_inf(3) l_inf(2)/l_inf(3) 1]';
+Ha = [1 0 0; 0 1 0; l_inf(1) l_inf(2) l_inf(3)];
+
+% ToDo: compute the transformed lines lr1, lr2, lr3, lr4
+
+lr1 = Ha'\l1;
+lr2 = Ha'\l2;
+lr3 = Ha'\l3;
+lr4 = Ha'\l4;
+
+
+%1. l1 l3 be the image of two lines that are orthogonal in the world.
+%MX = b
+M = [lr1(1)*lr3(1) lr1(1)*lr3(2)+lr1(2)*lr3(1); lr2(1)*lr4(1) lr2(1)*lr4(2)+lr2(2)*lr4(1)];
+b = [-lr1(2)*lr3(2) -lr2(2)*lr4(2)];
+X = M\b';
+
+% Set matrix S
+S = [X(1) X(2); X(2) 1];
+
+%Find K by applying Cholesky
+K = chol(S, 'lower');
+
+%Build matrix Ha_s and apply it to image and lines
+Hs_a = [K [0; 0]; [0 0] 1];
+Ha_s = inv(Hs_a);
+I3 = apply_H(I, Ha_s);
+
+lrr1 = Ha_s'\l1;
+lrr2 = Ha_s'\l2;
+lrr3 = Ha_s'\l3;
+lrr4 = Ha_s'\l4;
+
+figure; imshow(uint8(I3));
+hold on;
+t=1:0.1:1000;
+plot(t, -(lrr1(1)*t + lrr1(3)) / lrr1(2), 'y');
+plot(t, -(lrr2(1)*t + lrr2(3)) / lrr2(2), 'y');
+plot(t, -(lrr3(1)*t + lrr3(3)) / lrr3(2), 'y');
+plot(t, -(lrr4(1)*t + lrr4(3)) / lrr4(2), 'y');
+hold off
+
+%Compute normal vectors and angles from there
+
+n1 = [l1(1)/l1(3), l1(2)/l1(3)];
+n2 = [l2(1)/l2(3), l2(2)/l2(3)];
+n3 = [l3(1)/l3(3), l3(2)/l3(3)];
+n4 = [l4(1)/l4(3), l4(2)/l4(3)];
+
+cos_1 = (n1*n3')/(sqrt(n1*n1')*sqrt(n3*n3'))
+cos_2 = (n2*n4')/(sqrt(n2*n2')*sqrt(n4*n4'))
+cos_3 = (n1*n2')/(sqrt(n1*n1')*sqrt(n2*n2'))
+cos_4 = (n4*n3')/(sqrt(n4*n4')*sqrt(n3*n3'))
+
+%Angles for transformed lines
+
+nr1 = [lrr1(1)/lrr1(3), lrr1(2)/lrr1(3)];
+nr2 = [lrr2(1)/lrr2(3), lrr2(2)/lrr2(3)];
+nr3 = [lrr3(1)/lrr3(3), lrr3(2)/lrr3(3)];
+nr4 = [lrr4(1)/lrr4(3), lrr4(2)/lrr4(3)];
+
+
+cos_1_r = (nr1*nr3')/(sqrt(nr1*nr1')*sqrt(nr3*nr3'))
+cos_2_r = (nr2*nr4')/(sqrt(nr2*nr2')*sqrt(nr4*nr4'))
+cos_3_r = (nr1*nr2')/(sqrt(nr1*nr1')*sqrt(nr2*nr2'))
+cos_4_r = (nr4*nr3')/(sqrt(nr4*nr4')*sqrt(nr3*nr3'))
 
