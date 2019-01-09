23157d7e601341e6f1b45f40a11dadf67527e9e3
pablorodriper <parope2@gmail.com>
2019-01-09 01:42:52 +0100

    Comments

--- lab2/homography2d.m
+++ lab2/homography2d.m
@@ -5,18 +5,18 @@ function H = homography2d(x1_points, x2_points)
 [x1_norm, T1] = normalise2dpts(x1_points);
 [x2_norm, T2] = normalise2dpts(x2_points);
 
-% Assemble the n 3x9 matrices A_i
+% Assemble the 2nx9 matrices A_i
 n_points = length(x1_norm);
 A = zeros(2*n_points,9);    % SVD input must be 2-D
-%A = zeros(3*n_points,9);    % SVD input must be 2-D
+
 vector_zero = [0 0 0];
 for n = 1:n_points
     X = x1_norm(:,n)';
     x = x2_norm(1,n); y = x2_norm(2,n); w = x2_norm(3,n);
     A(3*n-1,:) = [vector_zero  -w*X         y*X];
     A(3*n,:) = [w*X          vector_zero  -x*X];
-    %A(3*n,:)   = [-y*X         x*X          vector_zero ];
 end
+
 % We compute the solution by SVD
 [U,D,V] = svd(A);
 
