398c5df8a99477d676d63f5960c04b0f68f545ec
pablorodriper <parope2@gmail.com>
2019-01-16 01:49:26 +0100

    Update .git ignore

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -3,6 +3,9 @@
 addpath('Data')
 addpath('sift')
 plot_figures = true;    % if false, reduce the number of plotted figures
+%iptsetpref('ImshowBorder','tight');
+%ax = gca; outerpos = ax.OuterPosition; ti = ax.TightInset;  left = outerpos(1) + ti(1); bottom = outerpos(2) + ti(2); ax_width = outerpos(3) - ti(1) - ti(3); ax_height = outerpos(4) - ti(2) - ti(4); ax.Position = [left bottom ax_width ax_height];
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Lab 2: Image mosaics
 
